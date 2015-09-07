91d4085d1a6805c2f05ce313ada6bd59c48a849d
nlajunior <nlajunior@gmail.com>
2015-09-07 20:39:52 -0300

    paineiss

--- acesso.php
+++ acesso.php
@@ -1,4 +1,33 @@
 <?php require_once("cabecalho.php");?>
-<?php require_once("logica-usuario.php");?>
+
     <p class = "text-success centralizar">Bem Vindo!</p>   
+
+<!-- Paineis-->
+    <div class="row my-row centralizar">
+        <div class="col-md-3" >
+            <img src="images/meta.jpg" ></br>
+            <span class="my-span">Metas</span></br>
+            <a href="consulta-vendedor.php" class="btn btn-primary">Visualizar</a>
+        </div>
+
+        <div class="col-md-3">
+            <img src="images/indicador.jpg"></br>
+            <span class="my-span">Indicadores</span></br>
+            <a href="#" class="btn btn-primary">Visualizar</a>
+        </div>
+ 
+        <div class="col-md-3">
+            <img src="images/treinamento.jpg" alt="..."></br> 
+            <span class="my-span">Treinamentos</span></br>
+            <a href="#" class="btn btn-primary">Gerar Relatório</a>
+        </div>
+
+        <div class="col-md-3 ">
+            <img src="images/user-t.jpg"></br>
+            <span class="my-span">Usuários</span></br>
+            <a href="#" class="btn btn-primary">Cadastrar</a>
+        </div>
+
+    </div>
+
 <?php require_once("rodape.php");?>
